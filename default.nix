7e7f2b1905f73a8e7bf243f741ef484aac937ecf
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2015-07-06 12:17:13 +0300

    nix:  factor shell.nix and default.nix

---
+++ default.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = (import <nixpkgs> {}).pkgs;
+  ghc  = pkgs.haskell.packages.ghc7101;
+  sdl2-newapi = pkgs.haskell.lib.overrideCabal ghc.sdl2 (oldAttrs: {
+    buildDepends = oldAttrs.buildDepends ++ [ ghc.linear ghc.text ghc.vector ];
+    src          = pkgs.fetchgit {
+      url    = https://github.com/haskell-game/sdl2;
+      rev    = "5c406b34c513615cbd3da1bb8ef60c1f465925c6";
+      sha256 = "13q9ic6d60qvbaqqcrrs1warj6nd30b38n4pm283j0dszv17vzkr";
+     };
+  });
+in
+{ mkDerivation, base, base-unicode-symbols, containers, diagrams, linear, netwire, pkgconfig, stdenv, vector }:
+mkDerivation {
+  pname        = "mood";
+  version      = "0.0.1";
+  src          = ./.;
+  isLibrary    = false;
+  isExecutable = true;
+  buildDepends = [ base base-unicode-symbols containers diagrams linear netwire pkgconfig sdl2-newapi vector
+                 ];
+  description  = "Graph-backed visual mind assistant";
+  license      = stdenv.lib.licenses.agpl3;
+}
--- shell.nix
+++ shell.nix
@@ -1,42 +1,32 @@
-let pkgs =  (import <nixpkgs> {}).pkgs;
-    sdl2-newapi = pkgs.haskell-ng.lib.overrideCabal pkgs.haskellngPackages.sdl2 (oldAttrs: {
-      src = pkgs.fetchgit {
-        url = https://github.com/haskell-game/sdl2;
-        rev = "5c406b34c513615cbd3da1bb8ef60c1f465925c6";
-        sha256 = "13q9ic6d60qvbaqqcrrs1warj6nd30b38n4pm283j0dszv17vzkr";
-       };
-      buildDepends = oldAttrs.buildDepends ++ [ pkgs.haskellPackages.linear pkgs.haskellPackages.text pkgs.haskellPackages.vector ];
-    });
-    ghc-mod-git = pkgs.haskell-ng.lib.overrideCabal pkgs.haskellngPackages.ghc-mod (oldAttrs: {
-      src = pkgs.fetchgit {
-        url = https://github.com/kazu-yamamoto/ghc-mod;
-        rev = "247e4e0e7616fe1fecc68fdcf80d6249ac4cee4f";
-        sha256 = "2a23271d0e6907351a246f095040ba18c3ab6bf1cba08a14338d701defa55474";
-       };
-      # the new ghc mod also requires some new dependencies. Add them to buildDepends:
-      buildDepends = oldAttrs.buildDepends ++ [ cabal-helper-new pkgs.haskellngPackages.cereal ];
-    });
-    cabal-helper-new = pkgs.haskell-ng.lib.overrideCabal pkgs.haskellngPackages.cabal-helper (oldAttrs: {
-      version = "0.3.2.0";
-      sha256 = "06igjmr0n8418wid1pr74cgvlsmwni7ar72g9bddivlbxax1pfli";
-    });
-  in
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, base, pkgconfig, containers, netwire, sdl2, stdenv, linear, vector, hoogle-index, halive, diagrams }:
-             mkDerivation {
-               pname = "mood";
-               version = "0.1.0.0";
-               src = ./.;
-               isLibrary = false;
-               isExecutable = true;
-               buildDepends = [ base containers netwire sdl2-newapi strace ltrace
-                                emacs git silver-searcher haskellngPackages.cabal-install cabal-helper-new ghc-mod-git
-                                linear vector pkgconfig hoogle-index halive diagrams
-                              ];
-               homepage = "https://github.com/_deepfire/mood";
-               description = "A mind-assisting graph substrate";
-               license = stdenv.lib.licenses.gpl3;
-             }) {};
-in
-  pkg.env
+{ nixpkgs ? import <nixpkgs> {}
+, compiler ? "ghc7101"
+}:
+let
+  pkgs = nixpkgs.pkgs;
+  ghc  = pkgs.haskell.packages.${compiler};
+  pkgf = import ./.;
+  drv  = ghc.callPackage pkgf {};
+  ghc-mod      = pkgs.haskell.lib.overrideCabal ghc.ghc-mod (oldAttrs: {
+    src = pkgs.fetchgit {
+      url = https://github.com/kazu-yamamoto/ghc-mod;
+      rev = "247e4e0e7616fe1fecc68fdcf80d6249ac4cee4f";
+      sha256 = "2a23271d0e6907351a246f095040ba18c3ab6bf1cba08a14338d701defa55474";
+     };
+    # the new ghc mod also requires some new dependencies. Add them to buildDepends:
+    buildDepends = oldAttrs.buildDepends ++ [ cabal-helper ghc.cereal ];
+  });
+  cabal-helper = pkgs.haskell.lib.overrideCabal ghc.cabal-helper (oldAttrs: {
+    version = "0.3.2.0";
+    sha256 = "06igjmr0n8418wid1pr74cgvlsmwni7ar72g9bddivlbxax1pfli";
+  });
+in with pkgs;
+  (pkgs.haskell.lib.addBuildTools drv [
+    ghc.cabal-install
+    ghc.halive
+    ghc.hoogle-index
+    ##
+    cabal-helper
+    ghc-mod
+    ##
+    emacs git ltrace silver-searcher strace
+  ]).env
