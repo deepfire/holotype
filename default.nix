0aad4dc3c6cf36e4b6bc8c84c37269c1d3f4be51
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2015-08-22 17:59:10 +0300

    nix:  update sdl2

--- default.nix
+++ default.nix
@@ -6,8 +6,8 @@ let
     buildDepends = [ ghc.linear ghc.text ghc.vector ];
     src          = pkgs.fetchgit {
       url    = https://github.com/deepfire/sdl2;
-      rev    = "b1e4b1c4c504139c6bd444a6696f5c0dcaa16846";
-      sha256 = "0w4q2fjrmanysk7v3p420799ph5k1ngjvwdb30ma57pkxlx5j7il";
+      rev    = "bb2c4b6b52b48497f3271fc880dd1a0b11623ef7";
+      sha256 = "0fcirg3g2kd9d001hzz60mililp79l36prh0nqpcw0fzk5zzp9y2";
      };
   });
 in
