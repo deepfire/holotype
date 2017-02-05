b8213507e8a00b0b369a3faa7c772f36d4abe0f0
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-02-05 19:45:26 +0300

    better preloader

--- preload-opengl-driver.sh
+++ preload-opengl-driver.sh
@@ -1,1 +1,2 @@
+sudo NIX_PATH=nixpkgs=/home/desktop/src/nixpkgs ~desktop/.nix-profile/bin/nix-build ~desktop/src/core/nix/opengl.nix -o /run/opengl-driver
 export LD_LIBRARY_PATH=/run/opengl-driver/lib
