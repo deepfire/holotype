3cf6daa1b2d7b510395a3453f18bd36a5e360b9b
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-03-02 18:20:25 +0300

    settings:  a global default DPI and FontMap

---
+++ src/HoloSettings.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DataKinds, GADTs #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+{-# LANGUAGE UnicodeSyntax #-}
+module HoloSettings
+where
+
+import qualified System.IO.Unsafe as UN
+
+import Flatland
+import HoloFont
+
+defaultFontPreferences ∷ FontPreferences PU
+defaultFontPreferences =
+  FontPreferences
+  [ ("default",     Left $ Alias "defaultSans" )
+  , ("defaultSans", Right $ [ FontReq "Aurulent Sans" "Regular" $ FSROutline (PUs 12) ])
+  , ("defaultMono", Right $ [ FontReq "Terminus"      "Regular" $ FSRBitmap  (PUs 12) LT ])
+  ]
+
+data Settings u where
+  Settings ∷
+    { dπ      ∷ DΠ
+    , fontmap ∷ FontMap u
+    } → Settings u
+
+defaultSettings ∷ IO (Settings PU)
+defaultSettings = do
+  let dπ = 96
+  fontmap ← makeFontMap dπ fmDefault defaultFontPreferences
+  pure Settings{..}
