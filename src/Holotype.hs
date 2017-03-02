31667c4475fa52cc09a60d31bb80f4528571b5a9
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-03-02 18:21:31 +0300

    holotype:  use the new Settings/FontMap mechanism to resolve fonts

--- src/Holotype.hs
+++ src/Holotype.hs
@@ -1,9 +1,9 @@
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Extra.Solver #-}
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE PackageImports #-}
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TupleSections #-}
@@ -50,6 +50,7 @@ import Flatland
 import HoloCanvas
 import HoloCube
 import HoloFont
+import HoloSettings
 import WindowSys
 
 
@@ -67,16 +68,10 @@ holotype = proc _ → do
                            Right r → r
         stream = rStream r streamDesc & fromMaybe (error $ "Silly invariant #1 failure.")
 
-    -- Once: define the visual style
-    let dπ        = 96
-        fontPrefs =
-          [ FontReq "Aurulent Sans" "Regular" $ FSROutline (PUs 12)
-          , FontReq "Terminus"      "Regular" $ FSRBitmap  (PUs 12) LT ]
-        fm        = fmDefault
-    (mFont ∷ Maybe (Font True PU), failures) ← chooseFont fm fontPrefs
-    let font = mFont &
-          fromMaybe (error $ printf "FATAL: no suitable font among requested: %s.  Failures: %s" (show fontPrefs) (show failures))
-    textSettings ← makeTextSettings fm dπ font
+    Settings{..} ← defaultSettings
+
+    let font = lookupFont' fontmap "defaultSans"
+    textSettings ← makeTextSettings fmDefault dπ font
     --
     let style = In (CanvasS textSettings)
                    (In (RRectS { rrCLBezel = coGray 1 1, rrCDBezel = coGray 0.1 0.5, rrCBorder = coGray 0.5 1, rrCBG = coOpaq 0.1 0.1 0.5
@@ -88,13 +83,13 @@ holotype = proc _ → do
 
     -- Once: fill the visual structure with data
     let content = "Process intero killed\
-                  Starting:\
-                  stack ghci --with-ghc intero '--docker-run-args=--interactive=true --tty=false' --no-build --no-load --ghci-options -odir=/home/deepfire/src/mood/.stack-work/intero/intero17462TiM --ghci-options -hidir=/home/deepfire/src/mood/.stack-work/intero/intero17462TiM mood\
-                  Intero 0.1.20 (GHC 8.0.1)\
-                  Type :intro and press enter for an introduction of the standard commands."
+                   Starting:\
+                   stack ghci --with-ghc intero '--docker-run-args=--interactive=true --tty=false' --no-build --no-load --ghci-options -odir=/home/deepfire/src/mood/.stack-work/intero/intero17462TiM --ghci-options -hidir=/home/deepfire/src/mood/.stack-work/intero/intero17462TiM mood\
+                   Intero 0.1.20 (GHC 8.0.1)\
+                   Type :intro and press enter for an introduction of the standard commands."
 
-    w ← assemble stream style ("lollestry", Wi 256)
-    render w
+    c ← assemble stream style ("lollestry", Wi 256)
+    render c
 
     -- Frame: GL setup
     GLFW.pollEvents
@@ -102,7 +97,7 @@ holotype = proc _ → do
         overbrightBits  = 0
     GL.uniformFloat "identityLight" slotU $ 1 / (2 ^ overbrightBits) -- used by lc:mkColor
     (screenW, screenH) ← GLFW.getFramebufferSize win
-    drawablePosition (drawableOf w) (Di $ V2 screenW screenH) (Po $ V2 (-0.25) (-0.2))
+    drawablePosition (drawableOf c) (Di $ V2 screenW screenH) (Po $ V2 (-0.25) (-0.2))
     GL.setScreenSize rGLStorage (fromIntegral screenW) (fromIntegral screenH)
     GL.renderFrame rGLRenderer
     GLFW.swapBuffers win
