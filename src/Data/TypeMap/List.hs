5eb92da7d638dc9b00551558e67517f7b21416d4
lyxia <lysxia@gmail.com>
2017-04-09 18:10:02 -0400

    Add List.toVector

--- src/Data/TypeMap/List.hs
+++ src/Data/TypeMap/List.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE PolyKinds #-}
+
 module Data.TypeMap.List
   ( TypeList
   , empty
   , index
   , cons
   , snoc
+  , toVector
   ) where
 
+import qualified Data.Vector as V
 import Data.TypeMap.Internal.List
+import Data.TypeMap.Internal.Vector (TypeVector(..))
+
+toVector :: TypeList d -> TypeVector d
+toVector (TypeList l) = TypeVector (V.fromList l)
