c3de43ecca8755b539108a3cc6fd8d281425221d
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-10-25 03:26:02 +0300

    elsewhere:  add pretty-printing aids & strict/lazy text compat tools

--- src/Elsewhere.hs
+++ src/Elsewhere.hs
@@ -12,6 +12,7 @@ import           Control.Monad.State
 import           Data.Complex
 import           Data.Glb                            (HasGlb(..))
 import           Data.Lub                            (HasLub(..))
+import           Data.Maybe                          (fromMaybe)
 import           Data.Monoid                         ((<>))
 import           Data.Text                           (Text)
 import qualified Data.Text                         as T
@@ -19,10 +20,14 @@ import qualified Data.Text.Lazy                    as TL
 import qualified Data.Text.Format                  as T
 import qualified Data.Text.Zipper                  as T
 import           Data.Void
+import           Debug.Trace                              (trace)
 import           GHC.Stack
-import           Linear
+import           Linear                            hiding (trace)
 import           Prelude.Unicode
 import           Reflex
+import qualified Data.Text.Lazy.IO                 as TLIO
+-- import           Text.PrettyPrint.Free             hiding ((<>), space)
+import           Text.PrettyPrint.Leijen.Text      hiding ((<>), (<$>), space)
 
 
 -- * Pretty numbers
@@ -79,7 +84,47 @@ someFire a b = simpler a <> simpler b
 
 
 -- * Pretty
-class Show a ⇒ Pretty a where
+dumpPretty ∷ Pretty a ⇒ Int → a → IO ()
+dumpPretty = TLIO.putStrLn .: ppPretty
+
+ppCompactS ∷ Pretty a ⇒ a → String
+ppCompactS = rendCompactS ∘ pretty
+
+ppCompact ∷ Pretty a ⇒ a → Text
+ppCompact = T.pack ∘ ppCompactS
+
+ppPrettyS ∷ Pretty a ⇒ Int → a → String
+ppPrettyS wi = TL.unpack ∘ ppPretty wi
+
+ppPretty ∷ Pretty a ⇒ Int → a → TL.Text
+ppPretty wi =  rendPretty wi ∘ pretty
+
+trace'pp ∷ Pretty a ⇒ String → a → a
+trace'pp prefix o = trace (prefix <> TL.unpack (ppPretty 60 o)) o
+
+rendCompact ∷ Doc → TL.Text
+rendCompact = displayT ∘ renderCompact
+
+rendCompactS ∷ Doc → String
+rendCompactS = TL.unpack ∘ rendCompact
+
+rendPrettyS ∷ Int → Doc → String
+rendPrettyS wi = TL.unpack ∘ rendPretty wi
+
+rendPretty ∷ Int → Doc → TL.Text
+rendPretty wi = displayT ∘ renderPretty 1.0 wi
+
+prettyMaybe ∷ Pretty a ⇒ TL.Text → Maybe a → Doc
+prettyMaybe m = fromMaybe (text m) ∘ (pretty <$>)
+
+unreadable ∷ TL.Text → Doc → Doc
+unreadable ty x = char '#' <> angles (text ty <+> x)
+
+(<->), (<:>) ∷ Doc → Doc → Doc
+l <-> r = l <> char '-' <> r
+l <:> r = l <> char ':' <> r
+
+class Show a ⇒ PP a where
   {-# MINIMAL pp | ppL #-}
   pp  ∷ a → Text
   ppL ∷ a → TL.Text
@@ -88,8 +133,15 @@ class Show a ⇒ Pretty a where
   ppL = TL.fromStrict ∘ pp
   ppS = T.unpack ∘ pp
 
-showT ∷ Show a ⇒ a → Text
-showT = T.pack ∘ show
+showTS, showT ∷ Show a ⇒ a → Text
+showTS = T.pack ∘ show
+showT = showTS
+
+showTL ∷ Show a ⇒ a → TL.Text
+showTL = TL.pack ∘ show
 
 errorT ∷ HasCallStack ⇒ Text → a
 errorT = error ∘ T.unpack
+
+errorTL ∷ HasCallStack ⇒ TL.Text → a
+errorTL = error ∘ TL.unpack
