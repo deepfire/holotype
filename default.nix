55c59d336c60804355d53d79bf48acbacca49259
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-11-06 07:10:09 +0300

    deps:  +monadplus, +tasty & the family

--- default.nix
+++ default.nix
@@ -5,13 +5,14 @@
 , gi-pangocairo, GLFW-b, Glob, hashable, haskell-gi-base, hedgehog
 , hspec, hxt, JuicyPixels, lambdacube-compiler, lambdacube-gl
 , lambdacube-ir, lambdacube-quake3, lens, linear, lub, metamorphic
-, MissingH, MonadRandom, mono-traversable, mtl, OpenGL, OpenGLRaw
-, pretty, pretty-show, profunctors, proteaaudio, QuickCheck, random
-, rapid, reflex, semigroupoids, semigroups, stdenv, stm, tasty
-, tasty-discover, tasty-hedgehog, tasty-hspec, tasty-hunit
-, tasty-quickcheck, tasty-smallcheck, text, text-format, text-lens
-, text-zipper, these, transformers, tuple, unordered-containers
-, vect, vector, wl-pprint-extras, wl-pprint-text
+, MissingH, monadplus, MonadRandom, mono-traversable, mtl, OpenGL
+, OpenGLRaw, pretty, pretty-show, profunctors, proteaaudio
+, QuickCheck, random, rapid, reflex, semigroupoids, semigroups
+, stdenv, stm, tasty, tasty-discover, tasty-expected-failure
+, tasty-hedgehog, tasty-hspec, tasty-hunit, tasty-quickcheck
+, tasty-smallcheck, text, text-format, text-lens, text-zipper
+, these, transformers, tuple, unordered-containers, vect, vector
+, wl-pprint-extras, wl-pprint-text
 }:
 mkDerivation {
   pname = "holotype";
@@ -23,14 +24,16 @@ mkDerivation {
     aeson base base-unicode-symbols binary bytestring cairo clock
     containers dependent-sum directory dlist exceptions extra filepath
     free ghc-prim ghc-typelits-extra ghc-typelits-natnormalise gi-cairo
-    gi-pango gi-pangocairo GLFW-b hashable haskell-gi-base hspec hxt
-    JuicyPixels lambdacube-compiler lambdacube-gl lambdacube-ir
-    lambdacube-quake3 lens linear lub metamorphic MissingH MonadRandom
-    mono-traversable mtl OpenGL OpenGLRaw pretty pretty-show
-    profunctors proteaaudio QuickCheck random rapid reflex
-    semigroupoids semigroups stm text text-format text-lens text-zipper
-    these transformers tuple unordered-containers vect vector
-    wl-pprint-extras wl-pprint-text
+    gi-pango gi-pangocairo GLFW-b hashable haskell-gi-base hedgehog
+    hspec hxt JuicyPixels lambdacube-compiler lambdacube-gl
+    lambdacube-ir lambdacube-quake3 lens linear lub metamorphic
+    MissingH monadplus MonadRandom mono-traversable mtl OpenGL
+    OpenGLRaw pretty pretty-show profunctors proteaaudio QuickCheck
+    random rapid reflex semigroupoids semigroups stm tasty
+    tasty-expected-failure tasty-hedgehog tasty-hspec tasty-hunit
+    tasty-quickcheck tasty-smallcheck text text-format text-lens
+    text-zipper these transformers tuple unordered-containers vect
+    vector wl-pprint-extras wl-pprint-text
   ];
   executableHaskellDepends = [
     aeson base base-unicode-symbols bytestring cairo clock containers
@@ -39,9 +42,10 @@ mkDerivation {
     lens linear mtl OpenGLRaw pretty-show text text-zipper vector
   ];
   testHaskellDepends = [
-    base containers directory filepath Glob hedgehog tasty
-    tasty-discover tasty-hedgehog tasty-hspec tasty-hunit
-    tasty-quickcheck tasty-smallcheck
+    base base-unicode-symbols containers directory filepath Glob
+    hedgehog lens tasty tasty-discover tasty-expected-failure
+    tasty-hedgehog tasty-hspec tasty-hunit tasty-quickcheck
+    tasty-smallcheck
   ];
   description = "Graph-backed visual mind assistant";
   license = stdenv.lib.licenses.agpl3;
