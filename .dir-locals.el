1e89b1b52f49796fa2ff7dd77c7c89c51df03d4f
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-08-09 01:54:54 +0300

    .dir-locals:  fix for dante

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil
+  .(
+    (dante-repl-command-line "cabal" "repl" "lib:holotype")
+    (haskell-interactive-tgt              . "lib:holotype")
+    (dante-target                         . "lib:holotype")
+    (dante-project-root .                     "~/holotype/src")
+    )))
