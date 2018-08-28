4a5c55e2f3ccca115e634f5025e7021b2a49dc0c
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-08-29 02:28:58 +0300

    pkgs:  update

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { mkDerivation, aeson, base, base-unicode-symbols, binary
 , bytestring, cairo, clock, containers, dependent-sum, directory
 , dlist, exceptions, extra, filepath, free, freer-simple, fsnotify
-, ghc-prim, gi-cairo, gi-pango, gi-pangocairo, GLFW-b, Glob
-, hashable, haskell-gi-base, hedgehog, hspec, hxt, JuicyPixels
-, lambdacube-compiler, lambdacube-gl, lambdacube-ir
+, ghc-prim, gi-cairo, gi-gobject, gi-pango, gi-pangocairo, GLFW-b
+, Glob, hashable, haskell-gi-base, hedgehog, hspec, hxt
+, JuicyPixels, lambdacube-compiler, lambdacube-gl, lambdacube-ir
 , lambdacube-quake3, lens, linear, lub, metamorphic, MissingH
 , monadplus, MonadRandom, mono-traversable, mtl, OpenGL, OpenGLRaw
 , parsers, pretty, pretty-show, primitive, profunctors, proteaaudio
@@ -24,13 +24,13 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson base base-unicode-symbols binary bytestring cairo clock
     containers dependent-sum directory dlist exceptions extra filepath
-    free freer-simple fsnotify ghc-prim gi-cairo gi-pango gi-pangocairo
-    GLFW-b hashable haskell-gi-base hedgehog hspec hxt JuicyPixels
-    lambdacube-compiler lambdacube-gl lambdacube-ir lambdacube-quake3
-    lens linear lub metamorphic MissingH monadplus MonadRandom
-    mono-traversable mtl OpenGL OpenGLRaw parsers pretty pretty-show
-    primitive profunctors proteaaudio QuickCheck random ref-tf reflex
-    semigroupoids semigroups singletons stm tasty
+    free freer-simple fsnotify ghc-prim gi-cairo gi-gobject gi-pango
+    gi-pangocairo GLFW-b hashable haskell-gi-base hedgehog hspec hxt
+    JuicyPixels lambdacube-compiler lambdacube-gl lambdacube-ir
+    lambdacube-quake3 lens linear lub metamorphic MissingH monadplus
+    MonadRandom mono-traversable mtl OpenGL OpenGLRaw parsers pretty
+    pretty-show primitive profunctors proteaaudio QuickCheck random
+    ref-tf reflex semigroupoids semigroups singletons stm tasty
     tasty-expected-failure tasty-hedgehog tasty-hspec tasty-hunit
     tasty-quickcheck tasty-smallcheck template-haskell text text-format
     text-lens text-zipper these transformers trifecta type-map
@@ -38,9 +38,10 @@ mkDerivation {
   ];
   executableHaskellDepends = [
     aeson base base-unicode-symbols bytestring cairo clock containers
-    directory free gi-cairo gi-pangocairo GLFW-b haskell-gi-base
-    lambdacube-compiler lambdacube-gl lambdacube-ir lambdacube-quake3
-    lens linear mtl OpenGLRaw pretty-show text text-zipper vector
+    directory free gi-cairo gi-gobject gi-pango gi-pangocairo GLFW-b
+    haskell-gi-base lambdacube-compiler lambdacube-gl lambdacube-ir
+    lambdacube-quake3 lens linear mtl OpenGLRaw pretty-show text
+    text-zipper vector
   ];
   testHaskellDepends = [
     base base-unicode-symbols containers directory filepath Glob
