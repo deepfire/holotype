128f05ccd181625f7593d117f302f012e07c7c28
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-09-12 22:39:24 +0300

    update.sh:  allow switching upstreams

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+who=${1:-deepfire}
+
 cabal2nix . > default.nix
 for x in ir gl compiler
-do nix-prefetch-git git@github.com:deepfire/lambdacube-$x > lambdacube-$x.src.json
+do nix-prefetch-git git@github.com:${who}/lambdacube-$x > lambdacube-$x.src.json
 done
