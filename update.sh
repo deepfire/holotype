7f8bf305e0d9c27ec88204d33f350c2a4650b475
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-09-21 03:11:17 +0300

    update.sh:  parametrise

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-who=${1:-deepfire}
+what="${1:-ir gl compiler}"
+who=${2:-deepfire}
 
 cabal2nix . > default.nix
-for x in ir gl compiler
+for x in ${what}
 do nix-prefetch-git git@github.com:${who}/lambdacube-$x > lambdacube-$x.src.json
 done
