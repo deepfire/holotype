d8addadb0c0ebd39984b8f88bace6bd08d3a960b
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-09-30 18:30:02 +0300

    update.sh:  generalise a bit

--- update.sh
+++ update.sh
@@ -13,5 +13,5 @@ do
 	then rbase=git@github.com:
 	else rbase=https://github.com/
         fi
-        nix-prefetch-git $rbase${who}/lambdacube-$x > lambdacube-$x.src.json
+        nix-prefetch-git $rbase${who}/$x > $x.src.json
 done
