e79e542f1ad3f10f905cd83be3a0c66cd509de27
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-09-30 17:46:48 +0300

    pkgs:  bump reflex

--- overrides.nix
+++ overrides.nix
@@ -22,14 +22,7 @@ with pkgs.haskell.lib; with self; {
 
   ## Upstreamed, awaiting a Hackage release
   lambdacube-compiler = overrideCabal super.lambdacube-compiler (drv: {
-    # src = pkgs.fetchFromGitHub {
-    #   owner  = "deepfire";
-    #   repo   = "lambdacube-compiler";
-    #   rev    = "e022a56bfeafe8306c7615fb1441364ccd876e8e";
-    #   sha256 = "0lfyr55jpk113bgkpv8k4140swdmnqh6cxsds9njm462gna3hffr";
-    # };
     src = pkgs.fetchgit (removeAttrs (builtins.fromJSON (builtins.readFile ./lambdacube-compiler.src.json)) ["date"]);
-    # src = ../lambdacube-compiler;
     libraryHaskellDepends = (drv.libraryHaskellDepends or []) ++ (with self; [ aeson semigroups exceptions megaparsec ansi-wl-pprint pretty-show lambdacube-ir vector ]);
     jailbreak       = true;
     enableLibraryProfiling = false;
@@ -37,26 +30,13 @@ with pkgs.haskell.lib; with self; {
 
   ## Upstreamed, awaiting a Hackage release
   lambdacube-gl = overrideCabal super.lambdacube-gl (drv: {
-    # src = pkgs.fetchFromGitHub {
-    #   owner  = "deepfire";
-    #   repo   = "lambdacube-gl";
-    #   rev    = "afa3956593e42ae0495615680f874292647612a3";
-    #   sha256 = "1s7y302qcdlwm9j0kzzk0k21drkyh9zijm6v4igr8kfixz2wmwll";
-    # };
     src = pkgs.fetchgit (removeAttrs (builtins.fromJSON (builtins.readFile ./lambdacube-gl.src.json)) ["date"]);
-    # src = ../lambdacube-gl;
     jailbreak       = true;
     enableLibraryProfiling = false;
   });
 
   ## Upstreamed, awaiting a Hackage release
   lambdacube-ir = overrideCabal super.lambdacube-ir (drv: {
-    # src = pkgs.fetchFromGitHub {
-    #   owner  = "deepfire";
-    #   repo   = "lambdacube-ir";
-    #   rev    = "36e2a9a208356657c4589869ab6ad8d60a1b2225";
-    #   sha256 = "0b3f11133lhy758chf48k4p9q1sp1zalriwzzqa3kfq6hzxd33w9";
-    # };
     src = pkgs.fetchgit (removeAttrs (builtins.fromJSON (builtins.readFile ./lambdacube-ir.src.json)) ["date"]);
     prePatch        = "cd lambdacube-ir.haskell; ";
     jailbreak       = true;
@@ -68,8 +48,8 @@ with pkgs.haskell.lib; with self; {
     src = pkgs.fetchFromGitHub {
       owner  = "reflex-frp";
       repo   = "reflex";
-      rev    = "39d9392b191ec85052eb8839f6c9f207f9e21885";
-      sha256 = "07rzrapnalzvl24nc9vgbzah8qpwqkd2v90ijzchwhlgk9y82lfb";
+      rev    = "7180ebfe283d973c5abc5ac851904f488c579a0f";
+      sha256 = "1p3gcjlkrz4vs2gsrnzlwqdpkb7wc3rz6adj65d02jdrghx81idj";
     };
     doCheck         = false;
     jailbreak       = true;
@@ -100,6 +80,17 @@ with pkgs.haskell.lib; with self; {
       rev    = "c4231e1423e5c81f2779acf4462eea8872f6a4e6";
       sha256 = "1r82rqpjkkkwm35wrqwwqkba4nbvilf7arcwqiacvwfz5zkp2gf6";
     };
+  });
+
+  ## Unmerged.  PR: https://github.com/bgamari/monoidal-containers/pull/0
+  monoidal-containers = overrideCabal super.monoidal-containers (drv: {
+    src = pkgs.fetchFromGitHub {
+      owner  = "obsidiansystems";
+      repo   = "monoidal-containers";
+      rev    = "79c25ac6bb469bfa92f8fd226684617b6753e955";
+      sha256 = "0j2mwf5zhz7cmn01x9v51w8vpx16hrl9x9rcx8fggf21slva8lf8";
+    };
+    libraryHaskellDepends = (drv.libraryHaskellDepends or []) ++ (with self; [ aeson these ]);
   });
 
   ## Unmerged.  PR: https://github.com/bos/text-format/pull/23
