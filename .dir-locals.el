56c2ed7976ba014154be64fc52adabf5eb82d026
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-11-08 03:32:38 +0300

    dante:  simplify setup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,1 @@
-((nil
-  .(
-    (dante-repl-command-line "cabal" "repl" "lib:holotype")
-    (haskell-interactive-tgt              . "lib:holotype")
-    (dante-target                         . "lib:holotype")
-    (dante-project-root .                     "~/holotype/src")
-    )))
+((nil . ((dante-repl-command-line "cabal" "new-repl"))))
