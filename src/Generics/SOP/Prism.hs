a7e2be288d240e5ae7d801bb250ff630736a3363
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-12-18 06:01:49 +0300

    WIP prisms:  more fumbling around

--- src/Generics/SOP/Prism.hs
+++ src/Generics/SOP/Prism.hs
@@ -61,6 +61,7 @@ module Generics.SOP.Prism (
 import Prelude hiding (id, (.), curry, uncurry, const, head, tail)
 import Control.Arrow
 import Control.Category
+import Data.SOP.NP
 import Data.Label.Mono (Lens)
 import Data.Label.Point (Iso(..))
 import qualified Data.Label.Mono as Lens
@@ -73,7 +74,7 @@ import Generics.SOP
 
 -- | GPrism generalizes a monomorphic lens by allowing for different categories
 -- for the getter and modifier
-data GPrism r w a b = GPrism (r a b) (w (w b b, a) a)
+data GPrism r w z b = GPrism (r z b) (w (w b b, z) z)
 
 instance (Category r, ArrowApply w) => Category (GPrism r w) where
   id = GPrism id app
@@ -113,7 +114,7 @@ toLens (GPrism f g) = Lens.lens f g
 glenses :: forall r w a xs. (Generic a, Code a ~ '[xs], Arrow r, ArrowApply w) => NP (GPrism r w a) xs
 glenses = case sList :: SList (Code a) of
             SCons -> hliftA (\l ->
-                               (l   :: GPrism r w                 (NP I xs) _) .
+                               (l   :: GPrism r w                 (NP I xs) _) . -- lens from constructor-pack to field
                                (sop :: GPrism r w   (SOP I '[xs]) (NP I xs)) .
                                (rep :: GPrism r w a (Rep      a)))
                      (np :: NP (GPrism r w (NP I xs)) xs)
@@ -121,46 +122,20 @@ glenses = case sList :: SList (Code a) of
 --             _     -> error "inaccessible"
 -- #endif
 
-type family Last (x :: [k]) where
-  Last (x: '[]) = x
-  Last (x:  xs) = Last xs
+gprisms :: forall r w y xss. (Generic y, Code y ~ xss, Arrow r, ArrowApply w) => NP (NP (GPrism r w y)) xss
+gprisms = let (POP x) = hliftA
+                        (\l ->
+                           (l    :: GPrism r w               (NS (NP I) xss) _) .
+                           (sop' :: GPrism r w   (SOP I xss) (NS (NP I) xss)) .
+                           (rep  :: GPrism r w y (Rep     y)))
+                        (POP (npp :: (Arrow r, ArrowApply w, All SListI xss) => NP (NP (GPrism r w (NS (NP I) xss))) xss))
+          in x
 
--- sop' :: (Arrow r, ArrowApply w) => GPrism r w (SOP f xss) (NP f (Last xss))
--- sop' = fromIso $ Iso
---   (let r :: SListI yss => NS (NP f) yss -> NP f (Last yss)
---        r x = case x of
---          Z x  -> (undefined :: ())
---          S xs -> undefined
---        -- r (Z x)  = x :* Nil
---        -- r (S xs) = undefined
---    in \(SOP x)-> r x)
---   (let w :: NP (NP f) xss -> NS (NP f) xss
---        w (x :* Nil) = Z x
---        w (x :* xs)  = S (w xs)
---    in SOP . w)
+sop  :: (Arrow r, ArrowApply w) => GPrism r w (SOP f '[xs]) (NP f xs)
+sop  = fromIso $ Iso (\(SOP (Z x)) -> x) (SOP . Z)
 
-sop' :: (Arrow r, ArrowApply w) => GPrism r w (SOP I yss) (NP I ys)
-sop' = undefined
-
--- NP (NP (GPrism r w a)) xss    -- gprisms table of prisms, final
---  - needs to be inductively built by destructuring sList / reconstructing via :*
--- NP (GPrism r w a) x           -- single row of prisms, before :*
-
-gprisms :: forall r w a xss. (Generic a, Code a ~ xss, Arrow r, ArrowApply w) => NP (NP (GPrism r w a)) xss
-gprisms = go (sList :: SList (Code a))
-  where
-    go :: (SListI yss, All SListI yss) => SList yss -> NP (NP (GPrism r w a)) yss
-    go x = case x of
-      SNil -> Nil
-      SCons ->
-        (hliftA (\l ->
-                   l .                                               -- ::                  GPrism r w               (NP I ys) y
-                   (undefined :: GPrism r w (SOP I yss) (NP I ys)) .
-                   rep)                                              -- ::                  GPrism r w a (Rep     a)
-         np)                                                         -- :: SListI ys => NP (GPrism r w (NP I ys)) ys
-                                                                     -- :: SListI ys => NP (GPrism r w a)         ys
-        :*
-        (go sList :: All SListI yss => NP (NP (GPrism r w a)) yss)
+sop' :: (Arrow r, ArrowApply w) => GPrism r w (SOP I xss) (NS (NP I) xss)
+sop' = fromIso $ Iso (\(SOP x)     -> x)  SOP
 
 {-------------------------------------------------------------------------------
   Generalized lenses for representation types
@@ -168,23 +143,8 @@ gprisms = go (sList :: SList (Code a))
 
 data CPrisms r w xs = CPrisms { unCPrisms :: NP (GPrism r w (NP I xs)) xs }
 
-np :: forall r w xs. (Arrow r, ArrowApply w, SListI xs) => NP (GPrism r w (NP I xs)) xs
-np = case sList :: SList xs of
-      SNil  -> Nil
-      SCons -> i . head :* hliftA (. tail) np
-
-nx :: forall r w xss. (Arrow r, ArrowApply w, All SListI xss) => NP (CPrisms r w) xss
-nx = case sList :: SList xss of
-       SNil  -> Nil
-       SCons -> CPrisms np :* nx
-
-rep :: (Arrow r, ArrowApply w, Generic a) => GPrism r w a (Rep a)
-rep = fromIso $ Iso from to
-
-sop  :: (Arrow r, ArrowApply w) => GPrism r w (SOP f '[xs]) (NP f xs)
-sop  = fromIso $ Iso
-  (\(SOP (Z x)) -> x)
-  (SOP . Z)
+i :: (Arrow r, ArrowApply w) => GPrism r w (I a) a
+i = fromIso $ Iso unI I
 
 head :: (Arrow r, ArrowApply w) => GPrism r w (NP f (x ': xs)) (f x)
 head = fromLens $ Lens.lens (\(x :* _) -> x) (\(f, x :* xs) -> (f x :* xs))
@@ -192,8 +152,70 @@ head = fromLens $ Lens.lens (\(x :* _) -> x) (\(f, x :* xs) -> (f x :* xs))
 tail :: (Arrow r, ArrowApply w) => GPrism r w (NP f (x ': xs)) (NP f xs)
 tail = fromLens $ Lens.lens (\(_ :* xs) -> xs) (\(f, x :* xs) -> (x :* f xs))
 
-i :: (Arrow r, ArrowApply w) => GPrism r w (I a) a
-i = fromIso $ Iso unI I
+-- this type sig seems fine, it's just unclear how to construct such an object..
+npp :: forall r w yss. (Arrow r, ArrowApply w, SListI yss, All SListI yss, All2 Top yss) => NP (NP (GPrism r w (NS (NP I) yss))) yss
+npp = case sList :: SList yss of
+      SNil  -> Nil
+      SCons ->
+        let hd = (
+              hliftA
+              -- ( i .
+              --   i . head
+              --   undefined
+              -- )
+              -- (undefined :: I a -> GPrism r w (NS (NP I) (x : xs)) a)
+              -- (undefined $ cpure_NP (Proxy @(All Top)) i)
+              (undefined)
+              (undefined)
+              )
+            hd :: (SListI xs) =>
+                  NP (GPrism r w (NS (NP I) (xs : xss1))) xs
+            --  list of lenses for the first constructor on the remainder of the list
+            tl :: (SListI (xs : xss1), All SListI (xs : xss1)) =>
+              NP (NP (GPrism r w (NS (NP I) (xs : xss1)))) xss1
+            --  table of lenses for the remaining constructors on the the list
+            tl = (
+              hliftA -- (Proxy @(All Top))
+                ((hliftA -- (Proxy @Top)
+                  ((\(x :: GPrism r w (NS (NP I) xss1) x) ->
+                      (.)
+                     x
+                     tail'
+                   )
+                   :: GPrism r w (NS (NP I) xss1) x
+                   -> GPrism r w (NS (NP I) (xs : xss1)) x
+                  )
+                 )
+                 :: NP (GPrism r w (NS (NP I) xss1)) xs
+                 -> NP (GPrism r w (NS (NP I) (xs : xss1))) xs
+                )
+                (npp
+                  :: All SListI xss1 =>
+                  NP (NP (GPrism r w (NS (NP I) xss1))) xss1
+                )
+              )
+        in hd :* tl
+
+tail' :: (Arrow r, ArrowApply w) => GPrism r w (NS (NP f) (x1 : xs1)) (NS (NP f) xs1)
+tail' = fromLens $ Lens.lens
+  (\(S xs) -> xs)
+  (\(f, S xs) -> (S $ f xs))
+
+-- hetero-list of lenses
+--     from a hetero-list of field values
+--     to values
+np :: forall r w xs. (Arrow r, ArrowApply w, SListI xs) => NP (GPrism r w (NP I xs)) xs
+np = case sList :: SList xs of
+      SNil  -> Nil
+      SCons ->
+        (i . head
+         ::                   GPrism r w (NP I (x : xs1)) x)
+        :*
+        (hliftA (. tail) np
+         :: SListI xs1 => NP (GPrism r w (NP I (x : xs1))) xs1)
+
+rep :: (Arrow r, ArrowApply w, Generic a) => GPrism r w a (Rep a)
+rep = fromIso $ Iso from to
 
 {-------------------------------------------------------------------------------
   Auxiliary
