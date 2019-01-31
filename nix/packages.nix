9a773bf0ae1a2d07bc1aa4ec1a17eb36c0a621cf
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2019-01-31 11:07:46 +0300

    Drop ./nix

--- nix/packages.nix
+++
@@ -1,8 +0,0 @@
-{ compiler    ? import ./default-compiler.nix
-, local       ? false
-, lib         ? import ./lib.nix
-, nixpkgs     ? lib.nixpkgs
-}:
-
-let
-  import ./nixpkgs.nix { inherit compiler local nixpkgs; } # :: nixpkgs/pkgs/development/haskell-modules/make-package-set.nix
--- nix/set-nixpkgs-rev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rev=$1
-upstream=${2:-NixOS}
-
-nix-prefetch-git --no-deepClone https://github.com/${upstream}/nixpkgs ${rev} > $(dirname $0)/pins/default-nixpkgs-src.json
